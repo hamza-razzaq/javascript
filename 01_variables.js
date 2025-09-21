10584059904645e65e8240b947582dd28bb8321e
hamza-razzaq <hamza.rzq99@gmail.com>
2025-09-21 22:58:10 +0500

    Add JavaScript files to 01_basics folder

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 122334 // Can't update
+let accountEmail = "hamza@gmail.com"
+var accountPass ="Hamza123"
+accountCity = "Lahore"
+let accountState
+
+
+accountEmail="hamza1@gmail.com"
+accountPass = "Hamza12345"
+accountCity = "Karachi"
+
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
