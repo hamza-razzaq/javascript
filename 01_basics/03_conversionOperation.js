1fa156f7ab10b3e5d8697934f8ebe08cda62500b
hamza-razzaq <hamza.rzq99@gmail.com>
2025-09-21 23:40:26 +0500

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,36 @@
-  let score="33abc"
-  console.log(typeof score)
-  console.log(typeof(score))
+//   let score="33abc"
+//   console.log(typeof score)
+//   console.log(typeof(score))
 
-  let valueInNumber = Number(score)
-  console.log(typeof valueInNumber)
-console.log(valueInNumber)
-console.log(typeof score)
+//   let valueInNumber = Number(score)
+//   console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// console.log(typeof score)
+
+//*******************Operations*************************
+
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%3)
+
+let str1 = "Hamza"
+let str2 = " Razzaq"
+
+let str3 = str1+str2
+
+console.log(str3)
+
+
+
+
+
+
