dd655bb5ae6a326075f0b65316cb135f4b85818f
hamza-razzaq <hamza.rzq99@gmail.com>
2025-10-03 04:07:20 +0500

    stack-heap

---
+++ 01_basics/stack-heap.js
@@ -0,0 +1,30 @@
+// Stack (PRIMITIVE), Heap (NON-PRIMITIVE)
+
+let myName= "Hamza Razzaq"
+let anotherName = myName
+ anotherName = "Ali"
+
+ console.log(myName);
+ console.log(anotherName);
+
+
+ let userOne = {
+    email:"hamza69@google.com",
+    age:22
+
+}
+
+let userTwo=userOne
+
+userTwo.email="hamza70@google.com"
+
+
+
+
+
+
+console.log(userOne)
+
+console.log(userTwo)
+ 
+
